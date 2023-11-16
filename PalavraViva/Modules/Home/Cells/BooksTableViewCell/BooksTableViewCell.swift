86c0e33a8b4cfc078f82a80fecb2ac1496fe281d
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-11-16 00:37:58 -0300

    add the collection view

--- PalavraViva/Modules/Home/Cells/BooksTableViewCell/BooksTableViewCell.swift
+++ PalavraViva/Modules/Home/Cells/BooksTableViewCell/BooksTableViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class BooksTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     static let identifier = String(describing: BooksTableViewCell.self)
     
@@ -25,8 +26,28 @@ class BooksTableViewCell: UITableViewCell {
         titleLabel.font = UIFont.boldSystemFont(ofSize: 32)
     }
     
+    func configCollection() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+            layout.estimatedItemSize = .zero
+        }
+    }
+    
     func setupCell(books: Books){
         titleLabel.text = books.title
     }
+}
+
+extension BooksTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
     
 }
