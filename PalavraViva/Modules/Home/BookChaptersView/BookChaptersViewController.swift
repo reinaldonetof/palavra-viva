f468d62fdba185101b7d8ffd77b668429c5447eb
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-12-01 23:30:15 -0300

    moveed models to outside, fixed the guard let at bookchapters, creation of praisesongsservice

--- PalavraViva/Modules/Home/BookChaptersView/BookChaptersViewController.swift
+++ PalavraViva/Modules/Home/BookChaptersView/BookChaptersViewController.swift
@@ -8,34 +8,34 @@
 import UIKit
 
 class BookChaptersViewController: UIViewController {
-    @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet var tableView: UITableView!
+
     var viewModel: BookChaptersViewModel
-    
+
     init?(coder: NSCoder, book: Book) {
-        self.viewModel = BookChaptersViewModel(book: book)
+        viewModel = BookChaptersViewModel(book: book)
         super.init(coder: coder)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configElements()
     }
-   
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
     }
-    
+
     func configElements() {
         let book = viewModel.getBook()
         title = book.name
@@ -50,20 +50,21 @@ extension BookChaptersViewController: UITableViewDelegate, UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.getNumberOfRows()
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let list = viewModel.getListToShow()
         if list[indexPath.row].type == ChaptersTableViewCell.identifier {
+            guard let book = list[indexPath.row].values as? Book else { return UITableViewCell() }
             let cell = tableView.dequeueReusableCell(withIdentifier: ChaptersTableViewCell.identifier, for: indexPath) as? ChaptersTableViewCell
             cell?.delegate = self
-            cell?.setupCell(book: list[indexPath.row].values as! Book)
+            cell?.setupCell(book: book)
             return cell ?? UITableViewCell()
         }
         return UITableViewCell()
     }
 }
 
-extension BookChaptersViewController:  ChaptersTableViewCellProtocol {
+extension BookChaptersViewController: ChaptersTableViewCellProtocol {
     func didSelectChapter(chapter: Int) {
         let vcString = String(describing: BookVersesTableViewController.self)
         let vc = UIStoryboard(name: vcString, bundle: nil).instantiateViewController(identifier: vcString) { coder -> BookVersesTableViewController? in
---
+++ PalavraViva/Modules/PraiseSongs/Service/PraiseSongsService.swift
@@ -0,0 +1,12 @@
+//
+//  PraiseSongsService.swift
+//  PalavraViva
+//
+//  Created by Reinaldo Neto on 01/12/23.
+//
+
+import Foundation
+
+class PraiseSongsService {
+    <#code#>
+}
