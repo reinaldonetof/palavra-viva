c7230ba9bb8bf6c10f2134ff8b176d9f59abe637
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-12-20 00:50:34 -0300

    storyboard completo do loginviewcontroller

--- PalavraViva/Modules/Login/LoginViewController.swift
+++ PalavraViva/Modules/Login/LoginViewController.swift
@@ -14,12 +14,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let vcString = String(describing: TabBarController.self)
-        let vc = UIStoryboard(name: vcString, bundle: nil).instantiateViewController(withIdentifier: vcString) as? TabBarController
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: { [weak self] in
-            guard let self else { return }
-            self.navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
-        })
+//        let vcString = String(describing: TabBarController.self)
+//        let vc = UIStoryboard(name: vcString, bundle: nil).instantiateViewController(withIdentifier: vcString) as? TabBarController
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: { [weak self] in
+//            guard let self else { return }
+//            self.navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+//        })
     }
 
 
