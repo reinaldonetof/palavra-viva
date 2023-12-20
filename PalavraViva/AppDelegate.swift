ad3e58fd1eaed495c413bacf8eaa4bd4c25acdb4
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-12-19 23:38:02 -0300

    added iqkeyboard

--- PalavraViva/AppDelegate.swift
+++ PalavraViva/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import FirebaseAuth
 import FirebaseCore
-//import FacebookCore
+import IQKeyboardManagerSwift
 import GoogleSignIn
 import UIKit
 
@@ -15,6 +15,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        IQKeyboardManager.shared.enable = true
         return true
     }
 
